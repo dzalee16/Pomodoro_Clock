3bb191fac56199f8cc764d56fc9c7de0331c6cde
dzalee16 <mladen.jovic93@yahoo.com>
2021-02-19 22:15:55 +0100

    Add custom.scss, create and style pomodoro page

--- src/pages/Pomodoro/index.js
+++ src/pages/Pomodoro/index.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import "../../scss/custom.scss";
 import "./style.scss";
 
 const Pomodoro = () => {
+  const [bgColor, setBgColor] = useState("pomodoro-container");
+
+  const changeBgColor = (e) => {
+    if (e.target.id === "pomodoro") {
+      console.log(e.target);
+      setBgColor("pomodoro-container");
+    } else if (e.target.id === "short-break") {
+      console.log(e.target);
+      setBgColor("short-break-container");
+    } else if (e.target.id === "long-break") {
+      console.log(e.target);
+      setBgColor("long-break-container");
+    }
+  };
+
   return (
     <main className="pomodoro">
-      <Container className="pomodoro-container">
+      <Container className={bgColor}>
         <Row className="d-flex justify-content-center">
-          <Col className="pomodoro-field" xs="auto">
-            Pomodoro
+          <Col className="pomodoro-field" xs="a">
+            <p id="pomodoro" onClick={changeBgColor}>
+              Pomodoro
+            </p>
           </Col>
           <Col className="pomodoro-field" xs="a">
-            Short Break
+            <p id="short-break" onClick={changeBgColor}>
+              Short Break
+            </p>
           </Col>
           <Col className="pomodoro-field" xs="a">
-            Long Break
+            <p id="long-break" onClick={changeBgColor}>
+              Long Break
+            </p>
           </Col>
         </Row>
         <Row className="pomodoro-timer">
@@ -24,7 +46,7 @@ const Pomodoro = () => {
         </Row>
         <Row className="pomodoro-timer">
           <Col className="text-center">
-            <Button className="pomodoro-btn">Start</Button>
+            <Button className="pomodoro-btn">START</Button>
           </Col>
         </Row>
       </Container>
